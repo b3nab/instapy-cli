cc9ebd54bd3a8e91a14cd57121e29a52689995f4
Ben <benedetto.abbenanti@gmail.com>
2017-10-10 06:49:06 +0200

    create README.rst for pip and bump version up :up:

--- setup.py
+++ setup.py
@@ -6,33 +6,28 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='instapy-cli',
-    version='0.0.1-dev0',
+    version='0.0.1',
     description='Python library and cli used to upload photo on Instagram. W/o a phone!',
-    long_description=open('README.md').read(),
+    long_description=open('README.md.rst').read(),
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        # How mature is this project?
+        'Development Status :: 5 - Production/Stable',
 
-        # Indicate who your project is intended for
+        # For who your project is intended for and its usage
         'Intended Audience :: Developers',
-        # 'Topic :: Software Development :: Build Tools',
+        'Environment :: Console',
 
-        # Pick your license as you wish (should match "license" above)
+        # Project's License
          'License :: OSI Approved :: MIT License',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
+        # Python versions instapy-cli support here
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        # 'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.2',
-        # 'Programming Language :: Python :: 3.3',
-        # 'Programming Language :: Python :: 3.4',
-        # 'Programming Language :: Python :: 3.5',
-        # 'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='instagram private upload api instapy instapy-cli instapycli',
     author='Benedetto Abbenanti',
