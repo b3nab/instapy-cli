aa46c819b2440a1b3571f9201c139d9fbeb98145
Ben <b3nab@users.noreply.github.com>
2019-04-06 21:47:21 +0200

    :fireworks: update example for story uplaod

--- examples/upload-a-photo-story.py
+++ examples/upload-a-photo-story.py
@@ -2,7 +2,7 @@ from instapy_cli import client
 
 username = 'USERNAME'
 password = 'PASSWORD'
-image = 'docs/image-sample-upload.jpg'
+image = 'docs/image-story-sample.jpg'
 
 with client(username, password) as cli:
     cli.upload(image, story=True)
